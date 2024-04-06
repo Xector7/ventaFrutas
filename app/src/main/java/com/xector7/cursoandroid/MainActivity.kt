5ef5c7912bff2e0ab28bcc9b9eceb550668d4415
Xector7 <xector7@xector.xecotr7@security.net>
2024-04-06 11:25:51 -0300

    commit inicial

---
+++ app/src/main/java/com/xector7/cursoandroid/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.xector7.cursoandroid
+
+import android.os.Bundle
+import android.widget.Spinner
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.xector7.cursoandroid.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+
+    }
+}
